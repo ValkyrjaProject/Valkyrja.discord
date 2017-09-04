96971c0333022ec3f90968b8ec750f01a3a52357
Radka Janek <radka.janek@redhat.com>
2017-09-04 02:58:15 +0200

    +Entities/CustomCommands

---
+++ Entities/CustomCommands.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+using guid = System.Int64;
+
+namespace Botwinder.entities
+{
+	[Table("custom_commands")]
+	public class CustomCommand
+	{
+		[Column("serverid")]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public guid ServerId = 0;
+
+		[Column("commandid", TypeName = "varchar(255)")]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public string CommandId = "";
+
+		[Column("response", TypeName = "text")]
+		public string Response = "This custom command was not configured.";
+
+		[Column("description", TypeName = "text")]
+		public string Description = "This is custom command on this server.";
+
+		[Column("delete_request")]
+		public bool DeleteRequest = false;
+	}
+
+	[Table("custom_aliases")]
+	public class CustomAlias
+	{
+		[Column("serverid")]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public guid ServerId = 0;
+
+		[Column("commandid", TypeName = "varchar(255)")]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public string CommandId = "";
+
+		[Column("alias", TypeName = "varchar(255)")]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public string Alias = "";
+	}
+}
