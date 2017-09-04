5082d830f0e04a46feb1c85143bf2918f12b64c8
Radka Janek <radka.janek@redhat.com>
2017-09-04 06:20:51 +0200

    +Entities/UserData

--- Entities/ServerContext.cs
+++ Entities/ServerContext.cs
@@ -20,6 +20,8 @@ namespace Botwinder.entities
 		public DbSet<CustomCommand> CustomCommands;
 		public DbSet<CustomAlias> CustomAliases;
 
+		public DbSet<UserData> UserDatabase;
+
 		public ServerContext(DbContextOptions<GlobalContext> options) : base(options)
 		{
 		}
@@ -27,17 +29,29 @@ namespace Botwinder.entities
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			modelBuilder.Entity<CommandOptions>()
-				.HasKey(p => new { p.ServerId, p.CommandId });
+				.HasKey(p => new{p.ServerId, p.CommandId});
 
 			modelBuilder.Entity<CommandChannelOptions>()
 				.HasKey(p => new{p.ServerId, p.CommandId, p.ChannelId});
 
 			modelBuilder.Entity<CustomCommand>()
-				.HasKey(p => new { p.ServerId, p.CommandId });
+				.HasKey(p => new{p.ServerId, p.CommandId});
 
 			modelBuilder.Entity<CustomAlias>()
-				.HasKey(p => new { p.ServerId, p.Alias });
+				.HasKey(p => new{p.ServerId, p.Alias});
 
+			modelBuilder.Entity<UserData>()
+				.HasKey(p => new{p.ServerId, p.UserId});
+
+			modelBuilder.Entity<Username>()
+				.HasOne(p => p.UserData)
+				.WithMany(p => p.Usernames)
+				.HasForeignKey(p => new{p.ServerId, p.UserId});
+
+			modelBuilder.Entity<Nickname>()
+				.HasOne(p => p.UserData)
+				.WithMany(p => p.Nicknames)
+				.HasForeignKey(p => new{p.ServerId, p.UserId});
 		}
 
 		public static GlobalContext Create(string connectionString)
