2589de7ab984b5ad30350d4cd77db1fcd029a0b9
Radka Janek <radka.janek@redhat.com>
2017-09-04 02:57:53 +0200

    +Entities/CommandOptions

---
+++ Entities/CommandOptions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+using guid = System.Int64;
+
+namespace Botwinder.entities
+{
+	[Table("command_options")]
+	public class CommandOptions
+	{
+		[Column("serverid")]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public guid ServerId = 0;
+
+		[Column("commandid", TypeName = "varchar(255)")]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public string CommandId = "";
+
+		[Column("permission_overrides", TypeName = "tinyint")]
+		public PermissionOverrides PermissionOverrides = PermissionOverrides.Default;
+
+		[Column("delete_request")]
+		public bool DeleteRequest = false;
+	}
+
+	[Table("command_channel_options")]
+	public class CommandChannelOptions
+	{
+		[Column("serverid")]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public guid ServerId = 0;
+
+		[Column("commandid", TypeName = "varchar(255)")]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public string CommandId = "";
+
+		[Column("channelidid")]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public guid ChannelId = 0;
+
+		[Column("blacklisted")]
+		public bool Blacklisted = false;
+
+		[Column("whitelisted")]
+		public bool Whitelisted = false;
+	}
+}
