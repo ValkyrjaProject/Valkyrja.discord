b44e81e679c0face1eed43e9e23d82d09c7e2c36
Radka Janek <radka.janek@redhat.com>
2017-09-22 04:55:41 +0200

    + an epic script

---
+++ setDiscordNetVersion.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+git rm packages/*
+mkdir packages
+mv ~/Downloads/Discord.Net.* packages/
+git add packages/
+
+sed -i "s/2.0.0-alpha-build-[0-9]*/2.0.0-alpha-build-$1/g" Core/Botwinder.core.csproj
+sed -i "s/2.0.0-alpha-build-[0-9]*/2.0.0-alpha-build-$1/g" Bot/Botwinder.discord.csproj
+sed -i "s/2.0.0-alpha-build-[0-9]*/2.0.0-alpha-build-$1/g" Modules/Botwinder.modules.csproj
+sed -i "s/2.0.0-alpha-build-[0-9]*/2.0.0-alpha-build-$1/g" Secure/Botwinder.secure.csproj
+
+git add Bot/Botwinder.discord.csproj
+git add Modules/Botwinder.modules.csproj
+git commit -m "D.NET Update"
+git push
+
+cd Core
+git add Botwinder.core.csproj
+git commit -m "D.NET Update"
+git push
+
+cd ../Secure
+git add Botwinder.secure.csproj
+git commit -m "D.NET Update"
+git push
+
