f5c86433d96d34bc912b909a221942cd3230c99a
Radka Janek <radka.janek@redhat.com>
2017-09-30 14:20:23 +0200

    *Moderation: Added placeholder commands.

--- Modules/Moderation.cs
+++ Modules/Moderation.cs
@@ -15,7 +15,7 @@ namespace Botwinder.secure
 
 		public async Task<List<Command<TUser>>> Init<TUser>(IBotwinderClient<TUser> iClient) where TUser : UserData, new()
 		{
-			//BotwinderClient<TUser> client = iClient as BotwinderClient<TUser>;
+			BotwinderClient<TUser> client = iClient as BotwinderClient<TUser>;
 			List<Command<TUser>> commands = new List<Command<TUser>>();
 
 // !op
@@ -53,17 +53,211 @@ namespace Botwinder.secure
 				}catch(Exception exception)
 				{
 					Discord.Net.HttpException ex = exception as Discord.Net.HttpException;
-					if( ex != null )
-					{
-						if( ex.HttpCode == System.Net.HttpStatusCode.Forbidden )
-							response = "Something went wrong, I may not have server permissions to do that.\n(Hint: Botwinder has to be above other roles to be able to manage them: <http://i.imgur.com/T8MPvME.png>)";
-					}
+					if( ex != null && ex.HttpCode == System.Net.HttpStatusCode.Forbidden )
+						response = "Something went wrong, I may not have server permissions to do that.\n(Hint: Botwinder has to be above other roles to be able to manage them: <http://i.imgur.com/T8MPvME.png>)";
 					else
-					{
-						await this.HandleException(exception, "op command", e.Server.Id);
-						response = "An unknown error has occurred.";
-					}
+						throw;
 				}
+				await iClient.SendMessageToChannel(e.Channel, response);
+			};
+			commands.Add(newCommand);
+
+// !mute
+			newCommand = new Command<TUser>("mute");
+			newCommand.Type = CommandType.Standard;
+			newCommand.Description = "Temporarily mute mentioned members from both the chat and voice. This command has to be configured at <http://botwinder.info/config>!";
+			newCommand.RequiredPermissions = PermissionType.ServerOwner | PermissionType.Admin | PermissionType.Moderator | PermissionType.SubModerator;
+			newCommand.OnExecute += async e => {
+				throw new NotImplementedException();
+				string response = "";
+				try
+				{
+				}catch(Exception exception)
+				{
+					Discord.Net.HttpException ex = exception as Discord.Net.HttpException;
+					if( ex != null && ex.HttpCode == System.Net.HttpStatusCode.Forbidden )
+						response = "Something went wrong, I may not have server permissions to do that.\n(Hint: Botwinder has to be above other roles to be able to manage them: <http://i.imgur.com/T8MPvME.png>)";
+					else
+						throw;
+				}
+				await iClient.SendMessageToChannel(e.Channel, response);
+			};
+			commands.Add(newCommand);
+
+// !munute
+			newCommand = new Command<TUser>("unmute");
+			newCommand.Type = CommandType.Standard;
+			newCommand.Description = "Unmute previously muted members. This command has to be configured at <http://botwinder.info/config>.";
+			newCommand.RequiredPermissions = PermissionType.ServerOwner | PermissionType.Admin | PermissionType.Moderator | PermissionType.SubModerator;
+			newCommand.OnExecute += async e => {
+				throw new NotImplementedException();
+				string response = "";
+				try
+				{
+				}catch(Exception exception)
+				{
+					Discord.Net.HttpException ex = exception as Discord.Net.HttpException;
+					if( ex != null && ex.HttpCode == System.Net.HttpStatusCode.Forbidden )
+						response = "Something went wrong, I may not have server permissions to do that.\n(Hint: Botwinder has to be above other roles to be able to manage them: <http://i.imgur.com/T8MPvME.png>)";
+					else
+						throw;
+				}
+				await iClient.SendMessageToChannel(e.Channel, response);
+			};
+			commands.Add(newCommand);
+
+// !kick
+			newCommand = new Command<TUser>("kick");
+			newCommand.Type = CommandType.Standard;
+			newCommand.Description = "Use with parameters `@user reason` where `@user` = user mention or id; `reason` = worded description why did you kick them out - they will receive this via PM.";
+			newCommand.RequiredPermissions = PermissionType.ServerOwner | PermissionType.Admin | PermissionType.Moderator;
+			newCommand.OnExecute += async e => {
+				throw new NotImplementedException();
+				string response = "";
+				try
+				{
+				}catch(Exception exception)
+				{
+					Discord.Net.HttpException ex = exception as Discord.Net.HttpException;
+					if( ex != null && ex.HttpCode == System.Net.HttpStatusCode.Forbidden )
+						response = "Something went wrong, I may not have server permissions to do that.\n(Hint: Botwinder has to be above other roles to be able to manage them: <http://i.imgur.com/T8MPvME.png>)";
+					else
+						throw;
+				}
+				await iClient.SendMessageToChannel(e.Channel, response);
+			};
+			commands.Add(newCommand);
+
+// !ban
+			newCommand = new Command<TUser>("ban");
+			newCommand.Type = CommandType.Standard;
+			newCommand.Description = "Use with parameters `@user time reason` where `@user` = user mention or id; `time` = duration of the ban (e.g. `7d` or `12h` or `0` for permanent.); `reason` = worded description why did you ban them - they will receive this via PM (use `silentBan` to not send the PM)";
+			newCommand.RequiredPermissions = PermissionType.ServerOwner | PermissionType.Admin | PermissionType.Moderator;
+			newCommand.OnExecute += async e => {
+				throw new NotImplementedException();
+				string response = "";
+				try
+				{
+				}catch(Exception exception)
+				{
+					Discord.Net.HttpException ex = exception as Discord.Net.HttpException;
+					if( ex != null && ex.HttpCode == System.Net.HttpStatusCode.Forbidden )
+						response = "Something went wrong, I may not have server permissions to do that.\n(Hint: Botwinder has to be above other roles to be able to manage them: <http://i.imgur.com/T8MPvME.png>)";
+					else
+						throw;
+				}
+				await iClient.SendMessageToChannel(e.Channel, response);
+			};
+			commands.Add(newCommand);
+
+			newCommand = newCommand.CreateCopy("silentban");
+			newCommand.Description = "Use with the same parameters like `ban`. The _reason_ message will not be sent to the user (hence silent.)";
+			commands.Add(newCommand);
+
+			newCommand = newCommand.CreateCopy("purgeban");
+			newCommand.Description = "Use with the same parameters like `ban`. The difference is that this command will also delete all the messages of the user in last 24 hours.";
+			commands.Add(newCommand);
+
+// !quickban
+			newCommand = new Command<TUser>("quickban");
+			newCommand.Type = CommandType.Standard;
+			newCommand.Description = "Quickly ban someone using pre-configured reason and duration, it also removes their messages. You can mention several people at once. (This command has to be first configured via `config` or <http://botwinder.info/config>.)";
+			newCommand.RequiredPermissions = PermissionType.ServerOwner | PermissionType.Admin | PermissionType.Moderator;
+			newCommand.OnExecute += async e => {
+				throw new NotImplementedException();
+				string response = "";
+				try
+				{
+				}catch(Exception exception)
+				{
+					Discord.Net.HttpException ex = exception as Discord.Net.HttpException;
+					if( ex != null && ex.HttpCode == System.Net.HttpStatusCode.Forbidden )
+						response = "Something went wrong, I may not have server permissions to do that.\n(Hint: Botwinder has to be above other roles to be able to manage them: <http://i.imgur.com/T8MPvME.png>)";
+					else
+						throw;
+				}
+				await iClient.SendMessageToChannel(e.Channel, response);
+			};
+			commands.Add(newCommand);
+
+// !unban
+			newCommand = new Command<TUser>("unban");
+			newCommand.Type = CommandType.Standard;
+			newCommand.Description = "Use with parameter `@user` where `@user` = user mention or id;";
+			newCommand.RequiredPermissions = PermissionType.ServerOwner | PermissionType.Admin | PermissionType.Moderator;
+			newCommand.OnExecute += async e => {
+				throw new NotImplementedException();
+				string response = "";
+				try
+				{
+				}catch(Exception exception)
+				{
+					Discord.Net.HttpException ex = exception as Discord.Net.HttpException;
+					if( ex != null && ex.HttpCode == System.Net.HttpStatusCode.Forbidden )
+						response = "Something went wrong, I may not have server permissions to do that.\n(Hint: Botwinder has to be above other roles to be able to manage them: <http://i.imgur.com/T8MPvME.png>)";
+					else
+						throw;
+				}
+				await iClient.SendMessageToChannel(e.Channel, response);
+			};
+			commands.Add(newCommand);
+
+// !addWarning
+			newCommand = new Command<TUser>("addWarning");
+			newCommand.Type = CommandType.Standard;
+			newCommand.Description = "Use with parameters `@user warning` where `@user` = user mention or id, you can add the same warning to multiple people, just mention them all; `warning` = worded description, a warning message to store in the database.";
+			newCommand.RequiredPermissions = PermissionType.ServerOwner | PermissionType.Admin | PermissionType.Moderator | PermissionType.SubModerator;
+			newCommand.OnExecute += async e => {
+				throw new NotImplementedException();
+				string response = "";
+				await iClient.SendMessageToChannel(e.Channel, response);
+			};
+			commands.Add(newCommand);
+			commands.Add(newCommand.CreateAlias("addwarning"));
+
+			newCommand = newCommand.CreateCopy("issueWarning");
+			newCommand.Description = "Use with parameters `@user warning` where `@user` = user mention or id, you can add the same warning to multiple people, just mention them all; `warning` = worded description, a warning message to store in the database. This will also be PMed to the user.";
+			commands.Add(newCommand);
+			commands.Add(newCommand.CreateAlias("issuewarning"));
+
+// !removeWarning
+			newCommand = new Command<TUser>("removeWarning");
+			newCommand.Type = CommandType.Standard;
+			newCommand.Description = "Use with parameter `@user` = user mention or id, you can remove the last warning from multiple people, just mention them all.";
+			newCommand.RequiredPermissions = PermissionType.ServerOwner | PermissionType.Admin | PermissionType.Moderator | PermissionType.SubModerator;
+			newCommand.OnExecute += async e => {
+				throw new NotImplementedException();
+				string response = "";
+				await iClient.SendMessageToChannel(e.Channel, response);
+			};
+			commands.Add(newCommand);
+			commands.Add(newCommand.CreateAlias("removewarning"));
+
+			newCommand = newCommand.CreateCopy("removeAllWarnings");
+			newCommand.Description = "Use with parameter `@user` = user mention or id, you can remove all the warnings from multiple people, just mention them all.";
+			commands.Add(newCommand);
+			commands.Add(newCommand.CreateAlias("removeallwarnings"));
+
+// !whois
+			newCommand = new Command<TUser>("whois");
+			newCommand.Type = CommandType.Standard;
+			newCommand.Description = "Search for a User on this server.";
+			newCommand.RequiredPermissions = PermissionType.ServerOwner | PermissionType.Admin | PermissionType.Moderator | PermissionType.SubModerator;
+			newCommand.OnExecute += async e => {
+				throw new NotImplementedException();
+				string response = "";
+				await iClient.SendMessageToChannel(e.Channel, response);
+			};
+			commands.Add(newCommand);
+
+// !find
+			newCommand = new Command<TUser>("find");
+			newCommand.Type = CommandType.Standard;
+			newCommand.Description = "Find a User in the database."; //todo - improve desc.
+			newCommand.RequiredPermissions = PermissionType.ServerOwner | PermissionType.Admin | PermissionType.Moderator | PermissionType.SubModerator;
+			newCommand.OnExecute += async e => {
+				throw new NotImplementedException();
+				string response = "";
 				await iClient.SendMessageToChannel(e.Channel, response);
 			};
 			commands.Add(newCommand);
