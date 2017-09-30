6497a834a4217c55b4f36e0e6f1ee12160a30cf7
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2017-09-30 14:06:22 -0700

    Created base Verification module

---
+++ Modules/Verification.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Botwinder.core;
+using Botwinder.entities;
+using guid = System.UInt64;
+
+namespace Botwinder.secure
+{
+    public class Verification : IModule
+    {
+        public Func<Exception, string, guid, Task> HandleException{ get; set; }
+
+        public async Task<List<Command<TUser>>> Init<TUser>(IBotwinderClient<TUser> iClient)
+            where TUser : UserData, new()
+        {
+            BotwinderClient<TUser> client = iClient as BotwinderClient<TUser>;
+            List<Command<TUser>> commands = new List<Command<TUser>>();
+
+            return commands;
+        }
+
+        public Task Update<TUser>(IBotwinderClient<TUser> iClient) where TUser : UserData, new()
+        {
+            BotwinderClient<TUser> client = iClient as BotwinderClient<TUser>;
+            throw new NotImplementedException();
+        }
+    }
+}
