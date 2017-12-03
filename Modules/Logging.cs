05b8a3fb688c98305600c16465883bfb25f394c7
Radka Janek <radka.janek@redhat.com>
2017-12-03 22:45:17 +0100

    *Logging: Removed mention from embeds.

--- Modules/Logging.cs
+++ Modules/Logging.cs
@@ -87,7 +87,7 @@ namespace Botwinder.modules
 						DateTime accountCreated = Utils.GetTimeFromId(user.Id);
 						await channel.SendMessageAsync("", embed:
 							GetLogSmolEmbed(new Color(server.Config.ActivityChannelColor),
-								string.Format(server.Config.LogMessageJoin, server.Config.LogMentionJoin ? $"<@{user.Id}>" : $"{user.GetUsername()}"),
+								string.Format(server.Config.LogMessageJoin, user.GetUsername()),
 								user.GetAvatarUrl(), $"UserId: {user.Id}",
 								"Account created: " + Utils.GetTimestamp(accountCreated), accountCreated));
 					}
@@ -124,7 +124,7 @@ namespace Botwinder.modules
 						DateTime accountCreated = Utils.GetTimeFromId(user.Id);
 						await channel.SendMessageAsync("", embed:
 							GetLogSmolEmbed(new Color(server.Config.ActivityChannelColor),
-								string.Format(server.Config.LogMessageLeave, server.Config.LogMentionLeave ? $"<@{user.Id}>" : $"{user.GetUsername()}"),
+								string.Format(server.Config.LogMessageLeave, user.GetUsername()),
 								user.GetAvatarUrl(), $"UserId: {user.Id}",
 								"Account created: " + Utils.GetTimestamp(accountCreated), accountCreated));
 					}
