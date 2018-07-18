1011cd5a7952b88f802195c0496b081744b3a9dc
Radka Janek <radka.janek@redhat.com>
2018-07-18 16:37:39 +0200

    profiles

--- Modules/Memo.cs
+++ Modules/Memo.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Botwinder.core;
 using Botwinder.entities;
+using Discord;
 using Discord.WebSocket;
 using guid = System.UInt64;
 
@@ -21,6 +24,10 @@ namespace Botwinder.modules
 
 		private BotwinderClient Client;
 
+		private readonly Regex ProfileParamRegex = new Regex("--?\\w+\\s(?!--?\\w|$).*?(?=\\s--?\\w|$)", RegexOptions.Compiled, TimeSpan.FromMilliseconds(100));
+		private readonly Regex ProfileOptionRegex = new Regex("--?\\w+", RegexOptions.Compiled, TimeSpan.FromMilliseconds(100));
+		private readonly Regex ProfileEmptyOptionRegex = new Regex("--?\\w+(?=\\s--?\\w|$)", RegexOptions.Compiled, TimeSpan.FromMilliseconds(100));
+
 
 		public Func<Exception, string, guid, Task> HandleException{ get; set; }
 
@@ -32,8 +39,8 @@ namespace Botwinder.modules
 // !memo
 			Command newCommand = new Command("memo");
 			newCommand.Type = CommandType.Standard;
-			newCommand.Description = "Display your memo. If used with a username or mention, it will display someone else' memo.";
-			newCommand.RequiredPermissions = PermissionType.Everyone;
+			newCommand.Description = "Display your memo. If used with a username or @mention, it will display someone else' memo.";
+			newCommand.RequiredPermissions = PermissionType.Member;
 			newCommand.OnExecute += async e => {
 				ServerContext dbContext = ServerContext.Create(this.Client.DbConnectionString);
 				string response = "";
@@ -72,13 +79,12 @@ namespace Botwinder.modules
 				dbContext.Dispose();
 			};
 			commands.Add(newCommand);
-			commands.Add(newCommand.CreateAlias("profile"));
 
 // !setMemo
 			newCommand = new Command("setMemo");
 			newCommand.Type = CommandType.Standard;
 			newCommand.Description = "Set your memo.";
-			newCommand.RequiredPermissions = PermissionType.Everyone;
+			newCommand.RequiredPermissions = PermissionType.Member;
 			newCommand.OnExecute += async e => {
 				ServerContext dbContext = ServerContext.Create(this.Client.DbConnectionString);
 				string response = "";
@@ -102,8 +108,146 @@ namespace Botwinder.modules
 			commands.Add(newCommand);
 			commands.Add(newCommand.CreateAlias("setmemo"));
 
+// !profile
+			newCommand = new Command("profile");
+			newCommand.Type = CommandType.Standard;
+			newCommand.Description = "Display your profile. If used with a username or @mention, it will display someone else' profile. Get Help: setProfile --help";
+			newCommand.RequiredPermissions = PermissionType.Everyone;
+			newCommand.OnExecute += async e => {
+				//TODO is profile enabled?
+				ServerContext dbContext = ServerContext.Create(this.Client.DbConnectionString);
+
+				if( !string.IsNullOrEmpty(e.TrimmedMessage) )
+				{
+					string expression = e.TrimmedMessage.ToLower();
+					SocketUser user = e.Message.MentionedUsers.FirstOrDefault();
+					if( user == null )
+						user = e.Server.Guild.Users.FirstOrDefault(u => (u?.Username != null && u.Username.ToLower() == expression) || (u?.Nickname != null && u.Nickname.ToLower() == expression));
+
+					if( user == null )
+					{
+						await e.SendReplySafe(NotFoundString);
+					}
+					else
+					{
+						await e.Channel.SendMessageAsync("", embed: GetProfileEmbed(dbContext, e.Server, user as SocketGuildUser));
+					}
+				}
+				else
+				{
+					await e.Channel.SendMessageAsync("", embed: GetProfileEmbed(dbContext, e.Server, e.Message.Author as SocketGuildUser));
+				}
+
+				dbContext.Dispose();
+			};
+			commands.Add(newCommand);
+
+// !setProfile
+			newCommand = new Command("setProfile");
+			newCommand.Type = CommandType.Standard;
+			newCommand.Description = "Set your profile. Get Help: setProfile --help";
+			newCommand.RequiredPermissions = PermissionType.Everyone;
+			newCommand.OnExecute += async e => {
+				//TODO is profile enabled?
+				ServerContext dbContext = ServerContext.Create(this.Client.DbConnectionString);
+				StringBuilder response = new StringBuilder();
+
+				IEnumerable<ProfileOption> options = dbContext.ProfileOptions.Where(o => o.ServerId == e.Server.Id).OrderBy(o => o.Order);
+				int maxOptionLength = 0;
+				foreach( ProfileOption option in options )
+				{
+					if( maxOptionLength < option.OptionAlt.Length )
+						maxOptionLength = option.OptionAlt.Length;
+				}
+
+				MatchCollection emptyOptions = this.ProfileEmptyOptionRegex.Matches(e.TrimmedMessage);
+
+				if( string.IsNullOrEmpty(e.TrimmedMessage) || (emptyOptions.Count > 0 && emptyOptions.Cast<Match>().Any(o => o.Value == "-h" || o.Value == "--help")) )
+				{
+					response.AppendLine("```md\nSet your profile fields with the following parameters:");
+					foreach( ProfileOption o in options )
+					{
+						response.Append($"  [ {o.Option} ][ {o.OptionAlt}");
+						response.Append(' ', maxOptionLength - o.OptionAlt.Length);
+						response.AppendLine($" ] | {o.Label}");
+					}
+
+					response.AppendLine($"\nExample:\n  {e.CommandId} --twitter @RheaAyase -y youtube.com/RheaAyase");
+					response.AppendLine($"\nTo null one of the options you have already set, leave it empty:\n  {e.CommandId} --twitter -y\n```");
+					dbContext.Dispose();
+					await e.SendReplySafe(response.ToString());
+					return;
+				}
+
+				foreach( Match match in emptyOptions )
+				{
+					ProfileOption option = options.FirstOrDefault(o => o.Option == match.Value || o.OptionAlt == match.Value);
+					if( option == null )
+						continue;
+					UserProfileOption userOption = dbContext.UserProfileOptions.FirstOrDefault(o => o.ServerId == e.Server.Id && o.UserId == e.Message.Author.Id && o.Option == option.Option);
+					if( userOption == null )
+						continue;
+
+					dbContext.UserProfileOptions.Remove(userOption);
+				}
+
+				MatchCollection matches = this.ProfileParamRegex.Matches(e.TrimmedMessage);
+				foreach( Match match in matches )
+				{
+					string optionString = this.ProfileOptionRegex.Match(match.Value).Value;
+					ProfileOption option = options.FirstOrDefault(o => o.Option == optionString || o.OptionAlt == optionString);
+					if( option == null )
+						continue;
+
+					UserProfileOption userOption = dbContext.UserProfileOptions.FirstOrDefault(o => o.ServerId == e.Server.Id && o.UserId == e.Message.Author.Id && o.Option == option.Option);
+					if( userOption == null )
+					{
+						userOption = new UserProfileOption(){
+							ServerId = e.Server.Id,
+							UserId = e.Message.Author.Id,
+							Option = option.Option
+						};
+						dbContext.UserProfileOptions.Add(userOption);
+					}
+
+					userOption.Value = match.Value.Substring(optionString.Length + 1);
+				}
+
+				dbContext.SaveChanges();
+
+				await e.Channel.SendMessageAsync("", embed: GetProfileEmbed(dbContext, e.Server, e.Message.Author as SocketGuildUser));
+
+				dbContext.Dispose();
+			};
+			commands.Add(newCommand);
+			commands.Add(newCommand.CreateAlias("setprofile"));
+
 
 			return commands;
+		}
+
+		private Embed GetProfileEmbed(ServerContext dbContext, Server server, SocketGuildUser user)
+		{
+			IEnumerable<ProfileOption> options = dbContext.ProfileOptions.Where(o => o.ServerId == server.Id).OrderBy(o => o.Order);
+
+			EmbedBuilder embedBuilder = new EmbedBuilder().WithThumbnailUrl(user.GetAvatarUrl())
+				.WithAuthor($"{user.GetNickname()}'s profile on {server.Guild.Name}", server.Guild.IconUrl)
+				.AddField("Username", user.GetUsername());
+
+			SocketRole highestRole = user.Roles.FirstOrDefault(r => r.Position == user.Hierarchy);
+			if( highestRole != null )
+				embedBuilder.Color = highestRole.Color;
+
+			foreach( ProfileOption option in options )
+			{
+				UserProfileOption userOption = dbContext.UserProfileOptions.FirstOrDefault(o => o.ServerId == server.Id && o.UserId == user.Id && o.Option == option.Option);
+				if( userOption == null || string.IsNullOrWhiteSpace(userOption.Value) )
+					continue;
+
+				embedBuilder.AddField(option.Label, userOption.Value);
+			}
+
+			return embedBuilder.Build();
 		}
 
 		public Task Update(IBotwinderClient iClient)
