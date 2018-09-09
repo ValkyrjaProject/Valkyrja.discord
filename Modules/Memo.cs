94a8281d909206b418cf39176f9316958770869b
Radka Janek <radka.janek@redhat.com>
2018-09-10 00:29:53 +0200

    +prev

--- Modules/Memo.cs
+++ Modules/Memo.cs
@@ -343,11 +343,11 @@ namespace Botwinder.modules
 				.WithAuthor($"My profile", user.GetAvatarUrl())
 				.AddField("Website", "[https://botwinder.info](https://botwinder.info)", true)
 				.AddField("Purpose", "[Community Management](http://rhea-ayase.eu/articles/2017-04/Moderation-guidelines)", true)
-				.AddField("Language", "[C#](https://en.wikipedia.org/wiki/C_Sharp_(programming_language))", true)
+				.AddField("Language", "[C#](https://en.wikipedia.org/wiki/C_Sharp_(programming_language\\))", true)
 				.AddField("Platform", "[.NET Core](https://github.com/dotnet)", true)
 				.AddField("License", "[MIT](https://github.com/RheaAyase/Botwinder.discord/blob/master/LICENSE)", true)
 				.AddField("Operating System", "[Fedora Linux](https://discord.gg/fedora)", true)
-				.AddField("Server", "Water cooled DualZeeon with 32x 3.8GHz, 64GB ECC memory, ~20TB raid5. [It's pink!](https://persephone.cloud)")
+				.AddField("Server", "Water cooled DualZeeon with 32x 3.8GHz, 64GB ECC memory and ~20TB raid5. [It's pink!](https://persephone.cloud)")
 				.AddField("Author", "A girl who inspires the desolate white space of Linux world with the delicate C# letters of simplified artificial intelligence. Also a [Mountain Biker](https://rhea-ayase.eu/mtb)")
 				.AddField("Web-Author", "[Her fiancé](https://github.com/SpyTec), also a professional slacker.")
 				.AddField("Questions?", "Direct them to [Jefi's Nest](https://discord.gg/XgVvkXx), Botwinder's support server.");
