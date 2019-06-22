288538903e92b0e5ae8510c7abb98a9a36bd83e0
Radka Janek <radka.janek@redhat.com>
2019-06-23 00:50:52 +0200

    D.NET Update

--- setDiscordNetVersion.sh
+++ setDiscordNetVersion.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-if [ "x$1" = "x" ]; then
-  echo "version please"
-  exit 1
-fi
+#if [ "x$1" = "x" ]; then
+#  echo "version please"
+#  exit 1
+#fi
 
 git rm packages/*
 mkdir packages
-mv ~/Downloads/Discord.Net.* packages/
-rm -rf ~/.nuget/packages/discord.net*
+#mv ~/Downloads/Discord.Net.* packages/
+#rm -rf ~/.nuget/packages/discord.net*
+find ~/dev/Discord.Net -name *.nupkg -exec cp {} packages/ \;
+rm packages/0*
 git add packages/
 
-sed -i "s/2.0.0-beta2-[0-9]*/2.0.0-beta2-$1/g" Core/Botwinder.core.csproj
-sed -i "s/2.0.0-beta2-[0-9]*/2.0.0-beta2-$1/g" Bot/Botwinder.discord.csproj
-sed -i "s/2.0.0-beta2-[0-9]*/2.0.0-beta2-$1/g" Modules/Botwinder.modules.csproj
-sed -i "s/2.0.0-beta2-[0-9]*/2.0.0-beta2-$1/g" Secure/Botwinder.secure.csproj
+sed -i "s/2.0.0-beta2-[0-9]*/2.2.0-dev-dev/g" Core/Botwinder.core.csproj
+sed -i "s/2.0.0-beta2-[0-9]*/2.2.0-dev-dev/g" Bot/Botwinder.discord.csproj
+sed -i "s/2.0.0-beta2-[0-9]*/2.2.0-dev-dev/g" Modules/Botwinder.modules.csproj
+sed -i "s/2.0.0-beta2-[0-9]*/2.2.0-dev-dev/g" Secure/Botwinder.secure.csproj
 
 git add Bot/Botwinder.discord.csproj
 git add Modules/Botwinder.modules.csproj
