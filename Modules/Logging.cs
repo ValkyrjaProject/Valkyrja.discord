6082e7b96bb713b903337ee24312dfb4bf04650e
RheaAyase <radka.janek@redhat.com>
2020-09-16 00:11:35 +0200

    *logging tweak

--- Modules/Logging.cs
+++ Modules/Logging.cs
@@ -534,13 +534,13 @@ namespace Valkyrja.modules
 						LogEmbed = GetLogEmbed(color, user?.GetAvatarUrl(), title, $"in [#{channel.Name}](https://discordapp.com/channels/{server.Id}/{channel.Id}/{message.Id})",
 							message.Author.GetUsername(), message.Author.Id.ToString(),
 							message.Id,
-							"Message", message.Content.Replace("@everyone", "@-everyone").Replace("@here", "@-here"),
-							message.Attachments.Any() ? "Files" : "", attachment.ToString()),
+							"Message", message.Content?.Replace("@everyone", "@-everyone").Replace("@here", "@-here") ?? " ",
+							message.Attachments?.Any() ?? false ? "Files" : "", attachment.ToString()),
 						LogString = GetLogMessage(title, "#" + channel.Name,
 							message.Author.GetUsername(), message.Author.Id.ToString(),
 							message.Id,
-							"Message", message.Content.Replace("@everyone", "@-everyone").Replace("@here", "@-here"),
-							message.Attachments.Any() ? "Files" : "", attachment.ToString())
+							"Message", message.Content?.Replace("@everyone", "@-everyone").Replace("@here", "@-here") ?? " ",
+							message.Attachments?.Any() ?? false ? "Files" : "", attachment.ToString())
 					};
 
 					await this.MessageQueueLock.WaitAsync();
@@ -588,17 +588,17 @@ namespace Valkyrja.modules
 					Message msg = new Message(){
 						Channel = logChannel,
 						DesiredType = (server.Config.LogChannelEmbeds) ? MessageType.Embed : MessageType.String,
-						LogEmbed = GetLogEmbed(new Color(red + green + blue), user?.GetAvatarUrl(),
+						LogEmbed = GetLogEmbed(new Color(red + green + blue), user.GetAvatarUrl(),
 							"Message Edited", $"in [#{channel.Name}](https://discordapp.com/channels/{server.Id}/{channel.Id}/{updatedMessage.Id})",
 							updatedMessage.Author.GetUsername(), updatedMessage.Author.Id.ToString(),
 							updatedMessage.Id,
-							"Before", originalMessage.Content.Replace("@everyone", "@-everyone").Replace("@here", "@-here"),
-							"After", updatedMessage.Content.Replace("@everyone", "@-everyone").Replace("@here", "@-here")),
+							"Before", originalMessage.Content?.Replace("@everyone", "@-everyone").Replace("@here", "@-here") ?? " ",
+							"After", updatedMessage.Content?.Replace("@everyone", "@-everyone").Replace("@here", "@-here") ?? " "),
 						LogString = GetLogMessage("Message Edited", "#" + channel.Name,
 							updatedMessage.Author.GetUsername(), updatedMessage.Author.Id.ToString(),
 							updatedMessage.Id,
-							"Before", originalMessage.Content.Replace("@everyone", "@-everyone").Replace("@here", "@-here"),
-							"After", updatedMessage.Content.Replace("@everyone", "@-everyone").Replace("@here", "@-here"))
+							"Before", originalMessage.Content?.Replace("@everyone", "@-everyone").Replace("@here", "@-here") ?? " ",
+							"After", updatedMessage.Content?.Replace("@everyone", "@-everyone").Replace("@here", "@-here") ?? " ")
 					};
 					await this.MessageQueueLock.WaitAsync();
 					this.MessageQueue.Add(msg);
