1039e03795e6bd74cc673c69694bc86ae517cd30
Rhea <contact@rhea.dev>
2026-06-19 20:38:18 +0100

    more timeout fixes

--- Modules/Moderation.cs
+++ Modules/Moderation.cs
@@ -1632,7 +1632,7 @@ namespace Valkyrja.modules
 			if( role != null )
 				await user.AddRoleAsync(role);
 			if( server.Guild.CurrentUser.GuildPermissions.ModerateMembers )
-				await user.SetTimeOutAsync(TimeSpan.FromSeconds(1));
+				await user.SetTimeOutAsync(TimeSpan.FromSeconds(10));
 
 			string warning = $"Muted {durationString}";
 			if( !string.IsNullOrEmpty(reason) )
@@ -1681,7 +1681,7 @@ namespace Valkyrja.modules
 					if( role != null )
 						await user.AddRoleAsync(role);
 					if( server.Guild.CurrentUser.GuildPermissions.ModerateMembers )
-						await user.SetTimeOutAsync(TimeSpan.FromSeconds(1));
+						await user.SetTimeOutAsync(TimeSpan.FromSeconds(10));
 
 					string warning = $"Muted {durationString}";
 					if( !string.IsNullOrEmpty(reason) )
